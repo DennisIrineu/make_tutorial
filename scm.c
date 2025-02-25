9d19311073aa701a8fea44bd2d79d3515930751f
DennisIrineu <dennispaulinoirineu@gmail.com>
2025-02-25 08:10:55 -0300

    adicionando função subtrair

--- scm.c
+++ scm.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
-void soma(int a, int b) {
+void soma(int a, int b)
+{
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-void multiplica(int a, int b) {
+void multiplica(int a, int b)
+{
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
-int main() {
+void subtrair(int a, int b)
+{
+    printf("%d - %d = %d\n", a, b, a - b);
+}
+
+int main()
+{
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    subtrair(5, 3);
     return 0;
 }
